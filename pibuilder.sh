15a705a725978b0ce69916d0b3cb3ebed9a607ac
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-02-15 18:49:36 +0100

    wow

--- pibuilder.sh
+++ pibuilder.sh
@@ -2,4 +2,4 @@
 
 GOOS=linux GOARCH=arm GOARM=6 go build -v /Users/ansi/development/go/src/github.com/ansgarschmidt/gosomatic/infofetcher/weatherunderground.go
 GOOS=linux GOARCH=arm GOARM=6 go build -v /Users/ansi/development/go/src/github.com/ansgarschmidt/gosomatic/infofetcher/radiation.go
-GOOS=linux GOARCH=arm GOARM=6 go build -v /Users/ansi/development/go/src/github.com/ansgarschmidt/gosomatic/infofetcher/worldofwarcraft.py.go
+GOOS=linux GOARCH=arm GOARM=6 go build -v /Users/ansi/development/go/src/github.com/ansgarschmidt/gosomatic/infofetcher/worldofwarcraft.go
